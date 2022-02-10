d6addc5a8146d5ed264a409f0b415d1698d31ea9
Falete <a21mardelcol@inspedralbes.cat>
2022-02-10 15:41:55 +0100

    4

--- 03.ps1
+++ 03.ps1
@@ -1,2 +1,2 @@
 #Crea la carpeta myTest des de PowerShell, al mateix lloc on et trobes
-New-Item "C:\SharedFolder" -itemType Directory
+New-Item "C:\Users\argo\Desktop\powershell---1-Falete-Marti-del-Moral-Coll" -itemType Directory
